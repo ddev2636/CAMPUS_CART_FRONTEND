0166a398024c672eed5c9387cc345a94f98b395e
Debaditya <ddev2636@gmail.com>
2024-10-10 21:06:11 +0530

    api-gateway

--- src/config.js
+++ src/config.js
@@ -2,11 +2,20 @@
 const isProduction = true;
 
 // Set the BASE_URL based on the environment
+// export const BASE_URL_PRODUCT = isProduction
+//   ? "http://ec2-3-6-87-233.ap-south-1.compute.amazonaws.com/service1"
+//   : "http://localhost:5000";
+// export const BASE_URL_AUTH = isProduction
+//   ? "http://ec2-3-6-87-233.ap-south-1.compute.amazonaws.com/service2"
+//   : "http://localhost:5001";
+// export const BASE_URL_CLIENT = isProduction
+//   ? "http://localhost:3000"
+//   : "http//localhost:3000";
 export const BASE_URL_PRODUCT = isProduction
-  ? "http://ec2-3-6-87-233.ap-south-1.compute.amazonaws.com/service1"
+  ? "https://n3clt0j1lc.execute-api.ap-south-1.amazonaws.com/prod/service1"
   : "http://localhost:5000";
 export const BASE_URL_AUTH = isProduction
-  ? "http://ec2-3-6-87-233.ap-south-1.compute.amazonaws.com/service2"
+  ? "https://n3clt0j1lc.execute-api.ap-south-1.amazonaws.com/prod/service2"
   : "http://localhost:5001";
 export const BASE_URL_CLIENT = isProduction
   ? "http://localhost:3000"
